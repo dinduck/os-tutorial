2053deaa816614006be3017969dddd02c816bf12
dingduck <kfove.liang@gmail.com>
2023-08-08 15:31:38 +0800

    Upload

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-fb01dfd20f2a739b.d
@@ -0,0 +1,9 @@
+/home/dingduck/Project/rs/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fb01dfd20f2a739b.rmeta: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+/home/dingduck/Project/rs/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fb01dfd20f2a739b.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-fc9e75045958323f.d
@@ -0,0 +1,9 @@
+/home/dingduck/Project/rs/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fc9e75045958323f: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+/home/dingduck/Project/rs/os/target/riscv64gc-unknown-none-elf/debug/deps/os-fc9e75045958323f.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
+
+src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
